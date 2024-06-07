0cb83816cbe9ac0106171ec446d2c465f59befd0
DiegoGC1822 <fenixly1029@gmail.com>
2024-06-06 20:06:50 -0500

    pruebas

---
+++ src/pages/EditBooks.jsx
@@ -0,0 +1,7 @@
+export const EditBooks = () => {
+    return (
+        <div>
+            <h1>EditBooks</h1>
+        </div>
+    )
+}
---
+++ src/pages/NewBooks.jsx
@@ -0,0 +1,7 @@
+export const NewBooks = () => {
+    return (
+        <div>
+            <h1>NewBooks</h1>
+        </div>
+    )
+}
