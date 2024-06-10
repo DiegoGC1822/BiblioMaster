19d603b00054398ecc7e1f6ea685e0db25bddc38
DiegoGC1822 <fenixly1029@gmail.com>
2024-06-09 20:35:51 -0500

    agregacion de estilo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,22 @@
 import { NavLink} from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useLocation } from "react-router-dom"
+import styles  from "../styles/Header.module.css"
 
 export const NavBar = ({ icons }) => {
+
+    const location = useLocation()
+
     return (
-        <div>
+        <ul className={styles.navbar}>
            {icons.map((icon, index) => (
                 <li key={index}>
-                    <NavLink to={icon.to}>
+                    <NavLink to={icon.to[0]} className={icon.to.includes(location.pathname) ? styles.active : ""}>
                         <FontAwesomeIcon icon={icon.class} />
                         <span>{icon.name}</span>
                     </NavLink>
                 </li>
             ))} 
-        </div>
+        </ul>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import db from '../db.json'
 import { useState } from 'react'
 import { useUserContext } from '../contexts/userContext'
+import styles from '../styles/Login.module.css'
 
 export const Login = () => {
 
@@ -22,7 +23,13 @@ export const Login = () => {
         if (user && user.value.password === password) {
             console.log('Login successful')
             setActualUser(user.value)
-            navigate('/newbooks')
+            if(user.value.role === 'Administrator'){
+                navigate('/newbook')
+                console.log(user)
+            } else {
+                navigate('/bookloan')
+                console.log(user)
+            }
         } else {
             console.log('Login failed')
         }
@@ -30,13 +37,15 @@ export const Login = () => {
     }
 
     return (
-        <div>
+        <div className={styles.login}>
+            <div style ={{backgroundColor: '#DCD7D7', padding: '30px', borderRadius: '10px'}}>
             <h1>BIBLIOMASTER</h1>
             <form onSubmit={handleLogin}>
                 <input type="text" onChange={e => setUsername(e.target.value)}/>
                 <input type="password" onChange={e => setPassword(e.target.value)}/>
                 <button>Entrar</button>
             </form>
+            </div>
         </div>
     )
 }
