e23c79fa9be960c95d9057971e91d49c164a35e8
DiegoGC1822 <fenixly1029@gmail.com>
2024-06-15 03:41:33 -0500

    creacion de nuevos usuarios

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,7 +1,82 @@
+import { useState } from "react"
+import { useUsersContext } from "../contexts/usersContext"
+
 export const NewUser = () => {
+
+    const {users, setUsers} = useUsersContext()
+
+    const [formState,setFormState] = useState({
+        username: "",
+        password: "",
+        role: ""
+    })    
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setFormState((prevFormState) => ({
+          ...prevFormState,
+          [name]: value,
+        }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        for (let key in formState) {
+          if (formState[key] === '') {
+            alert(`El campo ${key} está vacío`);
+            return
+          }
+        }
+        
+        setFormState((prevFormState) => ({
+            ...prevFormState,
+            id: users.length + 1
+        }))
+
+        setUsers((prevUsers) => ([
+            ...prevUsers,
+            formState
+        ]))
+
+        setFormState({
+            username: "",
+            password: "",
+            role: ""
+        })
+    }  
+
     return (
         <div>
-            <h1>newUser</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Nombre:</label>
+                    <input
+                    type="text"
+                    name="username"
+                    value={formState.username}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Email:</label>
+                    <input
+                    type="text"
+                    name="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="role">Rol:</label>
+                    <select htmlFor="role" name="role" value={formState.role} onChange={handleChange}>
+                        <option value="">Selecciona un rol</option>
+                        <option value="Administrator">Administrador</option>
+                        <option value="Estudent">Estudiante</option>
+                    </select>
+                </div>
+                <button type="submit">Enviar</button>
+            </form>   
         </div>
     )
 }
