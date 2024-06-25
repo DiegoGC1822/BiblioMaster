6777c32e2b08afea54d9b23dfcb29b91f78c1c51
DiegoGC1822 <fenixly1029@gmail.com>
2024-06-24 20:15:17 -0500

    requisito 6

--- src/algorithms/QuickSort.js
+++ src/algorithms/QuickSort.js
@@ -1,28 +1,39 @@
-function partition(head, end, criterion) {
-    let pivot = head;
-    let current = head.next;
-    let prev = head;
-  
-    while (current !== end) {
-      if (current.value[criterion] > pivot.value[criterion]) {
-        prev = prev.next;
-        [prev.value[criterion], current.value[criterion]] = [current.value[criterion], prev.value[criterion]];
+function partition(head, end, criterion, books, users) {
+  let pivot = head;
+  let current = head.next;
+  let prev = head;
+
+  const getValue = (node) => {
+      if (criterion === "bookTitle") {
+          const book = books.find(book => book.id === node.value.bookId)
+          return book ? book.title : ""
+      } else if (criterion === "username") {
+          const user = users.find(user => user.id === node.value.userId)
+          return user ? user.username : ""
       }
-      current = current.next;
-    }
-    [pivot.value[criterion], prev.value[criterion]] = [prev.value[criterion], pivot.value[criterion]];
-    return prev;
+      return node.value[criterion]
+  }
+
+  while (current !== end) {
+      if (getValue(current) < getValue(pivot)) {
+          prev = prev.next
+          ;[prev.value, current.value] = [current.value, prev.value]
+      }
+      current = current.next
+  }
+  ;[pivot.value, prev.value] = [prev.value, pivot.value]
+  return prev
 }
-  
-function quickSortRecursive(head, end, criterion) {
-    if (head !== end) {
-        const pivot = partition(head, end, criterion);
-        quickSortRecursive(head, pivot);
-        quickSortRecursive(pivot.next, end);
-    }
+
+function quickSortRecursive(head, end, criterion, books, users) {
+  if (head !== end) {
+      const pivot = partition(head, end, criterion, books, users)
+      quickSortRecursive(head, pivot, criterion, books, users)
+      quickSortRecursive(pivot.next, end, criterion, books, users)
+  }
 }
-  
-export default function quickSort(linkedList, criterion) {
-    quickSortRecursive(linkedList.head, null, criterion);
-    return linkedList
+
+export default function quickSort(linkedList, criterion, books, users) {
+  quickSortRecursive(linkedList.head, null, criterion, books, users)
+  return linkedList
 }
